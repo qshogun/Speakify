726874806909a0c6bc5b366a674c64d3bdc181a8
Piotr Kurszewski <pkurszewski@gmail.com>
2024-10-29 23:21:44 +0100

    Add DbContext to Program.cs

--- Speakify.API/Program.cs
+++ Speakify.API/Program.cs
@@ -1,6 +1,3 @@
-using Speakify.API.Repositories;
-using Speakify.API.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<ITimeEntryRepository, TimeEntryRepository>();
 builder.Services.AddScoped<ITimeEntryService, TimeEntryService>();
 
