b25e76e40045613ad62ab68b79224cfff0172167
Piotr Kurszewski <pkurszewski@gmail.com>
2024-10-30 00:58:09 +0100

    Modify result type

--- Speakify.API/Repositories/ITimeEntryRepository.cs
+++ Speakify.API/Repositories/ITimeEntryRepository.cs
@@ -7,7 +7,7 @@ public interface ITimeEntryRepository
     Task<List<TimeEntry>> GetAllTimeEntries();
     Task<TimeEntry?> GetTimeEntry(Guid id);
     Task<List<TimeEntry>> CreateTimeEntry(TimeEntry timeEntry);
-    Task<List<TimeEntry>?> DeleteTimeEntry(Guid id);
+    Task<List<TimeEntry?>> DeleteTimeEntry(Guid id);
     Task<List<TimeEntry>> DeleteAllTimeEntries();
     Task<TimeEntry?> UpdateTimeEntry(Guid id, TimeEntry updatedTimeEntry);
 }
