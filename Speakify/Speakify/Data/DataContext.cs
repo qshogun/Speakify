bcbd94a4ef6ef59d94d5ce79c790e46057a7ab92
Piotr Kurszewski <pkurszewski@gmail.com>
2024-10-31 22:44:31 +0100

    Add DataContext

---
+++ Speakify/Speakify/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Speakify.Models;
+
+namespace Speakify.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<BlogPost> BlogPosts { get; set; }
+}
