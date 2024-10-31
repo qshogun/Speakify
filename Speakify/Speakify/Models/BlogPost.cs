e41856f4e8bb07b53f117f7bd020a17d6d0952fd
Piotr Kurszewski <pkurszewski@gmail.com>
2024-10-31 22:44:06 +0100

    Add BlogPost class

---
+++ Speakify/Speakify/Models/BlogPost.cs
@@ -0,0 +1,10 @@
+﻿namespace Speakify.Models;
+
+public class BlogPost
+{
+    public Ulid Id { get; init; } = new Ulid();
+    public required string Title { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public DateTime? PublishedAtUtc { get; set; }
+    public bool IsPublished { get; set; } = false;
+}
